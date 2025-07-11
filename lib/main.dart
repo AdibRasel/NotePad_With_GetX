9c156adaf95112893a1191ed579f75778a56505d
Rasel Hossain Adib <97701792+AdibRasel@users.noreply.github.com>
2025-07-11 12:54:04 +0600

    File Folder Setup
    File Folder Setup

---
+++ lib/main.dart
@@ -0,0 +1,34 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: Home(),
+
+
+    );
+  }
+}
+
+
+class Home extends StatelessWidget {
+  const Home({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text("Node Pad", style: TextStyle(color: Colors.white),),
+        backgroundColor: Colors.blue,
+      ),
+    );
+  }
+}
